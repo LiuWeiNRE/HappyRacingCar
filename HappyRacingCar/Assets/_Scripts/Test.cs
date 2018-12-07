bb70235cbebe0ebc7a115528d444514dd07f6bb5
NRE <785807280@qq.com>
2018-12-07 23:25:15 +0800

    创建Test脚本

---
+++ HappyRacingCar/Assets/_Scripts/Test.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Test : MonoBehaviour 
+{
+    #region --变量定义
+
+    #endregion
+
+    #region --系统函数
+
+    #endregion
+
+    #region --自定义函数
+
+    #endregion
+}
