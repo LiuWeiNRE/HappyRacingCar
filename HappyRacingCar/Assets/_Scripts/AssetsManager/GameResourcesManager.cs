e354d6e5332caf171cdca5b9f5803e34607b3d2e
NRE <785807280@qq.com>
2018-12-09 16:03:37 +0800

    实现资源管理类

---
+++ HappyRacingCar/Assets/_Scripts/AssetsManager/GameResourcesManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class GameResourcesManager 
+{
+    #region --变量定义
+    public static string uiPath = "UIResources/UIPrefabs/";
+    #endregion
+
+    #region --系统函数
+
+    #endregion
+
+    #region --自定义函数
+    /// <summary>
+    /// 加载UI预制体
+    /// </summary>
+    /// <param name="pName"></param>
+    /// <returns></returns>
+    public static GameObject GetUIPrefab(string pName)
+    {
+        return ResourcesManager.Load<GameObject>(uiPath + pName);
+    }
+    #endregion
+}
---
+++ HappyRacingCar/Assets/_Scripts/AssetsManager/ResourcesManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ResourcesManager
+{
+    #region --变量定义
+    private static ResourcesManager instance;
+    #endregion
+
+    #region --属性
+    public static ResourcesManager Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = new ResourcesManager();
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    #region --自定义函数
+    /// <summary>
+    /// 从Resources文件夹下加载资源
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="assetPath"></param>
+    /// <returns></returns>
+    public static T Load<T>(string assetPath) where T : Object
+    {
+        return Resources.Load<T>(assetPath);
+    }
+    #endregion
+}
