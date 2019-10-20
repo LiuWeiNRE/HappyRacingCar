8f7f1b46a933652e9d665dd9069ee2dba446e81d
NRE <785807280@qq.com>
2019-10-20 23:02:17 +0800

    开发声音管理系统设置监听器功能，重置监听器功能，缓存清除功能。

--- HappyRacingCar/Assets/_Scripts/Core/Media/SoundGroup.cs
+++ HappyRacingCar/Assets/_Scripts/Core/Media/SoundGroup.cs
@@ -1,5 +1,4 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections.Generic;
 
 /*
  * 声音的播放标签，里面包含了同一个标签下的所有声音的播放。
@@ -12,6 +11,8 @@ public class SoundGroup
     private bool mute = false;  //分组内是否静音
     //分组内播放的声音，是否在声音加载完毕的时候才可以播放。通常背景音乐可以一边加载一边播放。
     private bool loadOverToPlay = false;
+    //记录所有的声音单元，注意最小的但是 SoundItem,且 key 和 value 都是 SoundItem 类型。
+    private Dictionary<SoundItem, SoundItem> allChannel = new Dictionary<SoundItem, SoundItem>();
     #endregion
 
     #region -- 系统函数
@@ -19,6 +20,10 @@ public class SoundGroup
     #endregion
 
     #region -- 自定义函数
-
+    internal void OnSoundOver(SoundItem _soundItem)
+    {
+        allChannel[_soundItem] = _soundItem;
+        _soundItem.SoundGroup = this;
+    }
     #endregion
 }
--- HappyRacingCar/Assets/_Scripts/Core/Media/SoundManager.cs
+++ HappyRacingCar/Assets/_Scripts/Core/Media/SoundManager.cs
@@ -6,10 +6,12 @@ using System.Collections;
  * 可以方便的对某一类声音进行停止,调整音量等操作。
  * 游戏中的声音可能分为音乐、音效。
  */
-public class SoundManager 
+public class SoundManager
 {
     #region -- 变量定义
-
+    internal static Hashtable allSound = new Hashtable();//保存所有的声音路径和组信息，allSound[url] = player
+    internal static GameObject soundPlayerObject;//常驻内存的声音对象
+    private static AudioListener audioListener;//声音监听器
     #endregion
 
     #region -- 系统函数
@@ -18,15 +20,70 @@ public class SoundManager
 
     #region -- 自定义函数
     /// <summary>
+    /// 设置监听器对象。如果该物体上没有监听器，就在该物体上新增一个 AudioListener 组件。
+    /// </summary>
+    /// <param name="_target"></param>
+    public static void SetListener(GameObject _target)
+    {
+        if (audioListener != null)
+        {
+            GameObject.Destroy(audioListener);
+        }
+        audioListener = _target.GetComponent<AudioListener>();
+        if (audioListener == null)
+        {
+            audioListener = _target.AddComponent<AudioListener>();
+        }
+    }
+    /// <summary>
+    /// 设置监听器对象。
+    /// </summary>
+    /// <param name="_audioListener"></param>
+    public static void SetListener(AudioListener _audioListener)
+    {
+        if (audioListener != null)
+        {
+            GameObject.Destroy(audioListener);
+        }
+        audioListener = _audioListener;
+    }
+    /// <summary>
     /// 重置监听器 AudioListener 组件，使场景中只存在一个监听器。
     /// 如果存在 MainCamera ,将监听器放到 MainCamera 上，否则将监听器放到 SoundPlayerObject 上。
     /// </summary>
     public static void ResetListener()
-    { }
+    {
+        Camera[] _cameras = Camera.allCameras;
+        foreach (Camera item in _cameras)
+        {
+            AudioListener _audioListener = item.gameObject.GetComponent<AudioListener>();
+            if (_audioListener != null)
+            {
+                SetListener(_audioListener);
+            }
+        }
+
+        if (audioListener == null)
+        {
+            if (Camera.main != null)
+            {
+                SetListener(Camera.main.gameObject);
+            }
+            else
+            {
+                SetListener(soundPlayerObject);
+            }
+        }
+    }
     /// <summary>
     /// 释放所有声音中Player中的内存
     /// </summary>
     public static void GC()
-    { }
+    {
+        foreach (SoundPlayer item in allSound)
+        {
+            item.GC();
+        }
+    }
     #endregion
 }
