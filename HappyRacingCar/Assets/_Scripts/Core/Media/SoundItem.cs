8a7e9614af48f9f4d09b069971eb7f957252437a
NRE <785807280@qq.com>
2019-10-20 23:04:13 +0800

    新增的类未受版本控制，重新提交一次

---
+++ HappyRacingCar/Assets/_Scripts/Core/Media/SoundItem.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+/*
+ * 一个声音播放通道
+ */
+public class SoundItem
+{
+    #region -- 变量定义
+    private SoundGroup soundGroup;//SoundItem属于的Group对象
+    private AudioSource audioSource;//声音源对象
+    #endregion
+
+    #region -- 自定义函数
+    internal SoundGroup SoundGroup
+    {
+        get { return soundGroup; }
+        set { soundGroup = value; }
+    }
+    public AudioSource AudioSource
+    {
+        get { return audioSource; }
+    }
+    public bool IsOver()
+    {
+        return audioSource == null;
+    }
+    #endregion
+}
---
+++ HappyRacingCar/Assets/_Scripts/Core/Media/SoundPlayer.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 一个声音的播放器，能播放一个声音文件。
+ * 资源管理类：资源的加载和循环利用内存的技术--Pool池技术
+ */
+public class SoundPlayer
+{
+    #region -- 变量定义
+    internal int soundCount = 0;//声音播放的计数器
+    //记录所有的声音Item值
+    private Dictionary<SoundItem, SoundItem> allChannel = new Dictionary<SoundItem, SoundItem>();
+    private List<AudioSource> unUsePool;//未使用的Pool池
+    internal GameObject playObject;//游戏对象节点：播放声音的对象
+    #endregion
+
+    #region -- 自定义函数
+    internal void OnSoundOver(SoundItem _soundItem, bool _remove = true)
+    {
+        soundCount--;
+        GameObject _go = _soundItem.AudioSource.gameObject;
+
+        if (_go == playObject)
+        {
+            unUsePool.Add(_soundItem.AudioSource);
+            _soundItem.AudioSource.enabled = false;
+        }
+        else if (_go.name == "SoundClip")//具体看Item的初始化代码
+        {
+            GameObject.DestroyObject(_go);
+        }
+        else
+        {
+            GameObject.Destroy(_soundItem.AudioSource);
+        }
+
+        if (_soundItem.SoundGroup != null)
+        {
+            _soundItem.SoundGroup.OnSoundOver(_soundItem);
+        }
+
+        if (_remove)
+        {
+            allChannel.Remove(_soundItem);
+        }
+    }
+    public void GC()
+    {
+        if (allChannel.Count == 0)
+        {
+            return;
+        }
+
+        List<SoundItem> _soundItems = new List<SoundItem>();
+        foreach (KeyValuePair<SoundItem, SoundItem> item in allChannel)
+        {
+            if (item.Value.IsOver())
+            {
+                _soundItems.Add(item.Key);
+            }
+            else if (!item.Value.AudioSource.isPlaying)
+            {
+                _soundItems.Add(item.Key);
+                OnSoundOver(item.Key, false);
+            }
+        }
+
+        int _count = _soundItems.Count;
+        if (_count != 0)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                allChannel.Remove(_soundItems[i]);
+            }
+        }
+    }
+    #endregion
+}
